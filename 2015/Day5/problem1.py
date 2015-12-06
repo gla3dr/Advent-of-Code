a96c7ec1a44faec5572a18afe8d9656120347792
gla3dr <zackmayoh@shaw.ca>
2015-12-05 18:14:32 -0600

    Add Day 5 solutions.

---
+++ 2015/Day5/problem1.py
@@ -0,0 +1,12 @@
+import re
+nice = 0
+naughty = ('ab', 'cd', 'pq', 'xy')
+vowels = ('a', 'e', 'i', 'o', 'u')
+dubs = re.compile(r'.*(.)\1.*')
+with open('input.txt') as input:
+    for word in input:
+        if not any(x in word for x in naughty):
+            if sum(letter in vowels for letter in word) >= 3:
+                if dubs.match(word):
+                    nice += 1
+    print nice
---
+++ 2015/Day5/problem2.py
@@ -0,0 +1,9 @@
+import re
+nice = 0
+prop1 = re.compile(r'.*(..).*\1.*')
+prop2 = re.compile(r'.*(.).\1.*')
+with open('input.txt') as input:
+    for word in input:
+        if prop1.match(word) and prop2.match(word):
+            nice += 1
+    print nice
