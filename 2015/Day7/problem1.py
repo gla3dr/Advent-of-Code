812e28ae9906dc78457a544350f4f6c4f6cf607b
gla3dr <zackmayoh@shaw.ca>
2015-12-07 16:46:20 -0600

    Try recursive solution.

--- 2015/Day7/problem1.py
+++ 2015/Day7/problem1.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 funcs = {
     'SIG' : (lambda in1, in2: in1),
     'AND' : (lambda in1, in2: in1 & in2),
@@ -8,31 +7,39 @@ funcs = {
     'RSHIFT' : (lambda in1, in2: in1 >> in2)
 }
 
-wires = defaultdict(int) 
+wires = {} 
 
-def get_operand(op):
+def solve(operation):
+    v1 = None
+    v2 = None
     try:
-        return int(op)
+        v1 = int(operation[0])
     except ValueError:
-        return wires[op]
+        v1 = int(solve(wires[operation[0]]))
+    if operation[2] is not None:
+        try:
+            v2 = int(operation[2])
+        except ValueError:
+            v2 = int(solve(wires[operation[2]]))
+    return funcs[operation[1]](v1, v2)
 
-with open('test_input.txt') as input:
+
+with open('input.txt') as input:
     for line in input:
         parts = line.split()
         operand1 = None
         operand2 = None
         operator = None
-        result = None
+        result = parts[-1]
         if len(parts) == 3:
-            operand1 = get_operand(parts[0])
+            operand1 = parts[0]
             operator = 'SIG'
         elif len(parts) == 4:
-            operand1 = get_operand(parts[1])
+            operand1 = parts[1]
             operator = parts[0]
         elif len(parts) == 5:
-            operand1 = get_operand(parts[0])
-            operand2 = get_operand(parts[2])
+            operand1 = parts[0]
+            operand2 = parts[2]
             operator = parts[1]
-        result = parts[-1]
-        wires[result] = funcs[operator](operand1, operand2)
-print wires
+        wires[result] = (operand1, operator, operand2)
+print solve(wires['a'])
