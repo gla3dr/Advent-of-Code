90414d7b5a0c1b301665ab0e9b2f0d628da6a4d5
gla3dr <zmayoh@gmail.com>
2017-12-01 01:36:39 -0600

    Actually fix formatting

--- 2017/Day1/problem1.py
+++ 2017/Day1/problem1.py
@@ -4,4 +4,3 @@ with open('2017/Day1/input.txt', 'r') as f:
         total = sum([int(c) for c, n in izip(l, islice(l, 1, None)) if int(c) == int(n) ])
         total = total + int(l[-1]) if int(l[-1]) == int(l[0]) else total
         print total
-        
