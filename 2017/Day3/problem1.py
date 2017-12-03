1e0d3ad9eae65b836f1056e7a35cc772bb49e666
gla3dr <zmayoh@gmail.com>
2017-12-03 07:03:42 -0600

    Add more documentation

--- 2017/Day3/problem1.py
+++ 2017/Day3/problem1.py
@@ -2,19 +2,44 @@
 from math import ceil
 
 def get_angle_from_index(i, r):
-    """ Calculate the angle of the given index (i) in the ring of the given radius (r).
+    """ Calculate the angle of the given index (`i`) in the ring of the given radius (`r`).
 
     This function only works for `r >= 2`.
 
-    Angle (θ) is calculated by the following function where `s = r - 1` (The ring indices are rotated by -1):
+    Angle (`θ`) is calculated by the following function where `s = r - 1` (The ring indices are rotated by -1):
+    ```
+    θ = ((-1)^(i/r))(s + (2⌈i/2r⌉)r) - ((-1)^(i/r))i
+    ```
 
-    `θ = ((-1)^(i/r))(s + (2⌈i/2r⌉)r) - ((-1)^(i/r))i`
+    This is the function for the graph that looks like this:
+    ```
+
+       max θ |    /\    /\    /\    /
+    θ        |\  /  \  /  \  /  \  /
+           0 |_\/____\/____\/____\/__
+         i = 0  s   s+2r  s+4r  s+6r
+    ```
+    The graph is translated in order to account for the rings' 0-th position being one up from the bottom right corner.
+
+    This allows you to easily determine the "angle" away from 0 at any index in any ring in this pattern:
+    ```
+    4 3 2 1 0 1 2 3 4
+    3 3 2 1 0 1 2 3 3
+    2 2 2 1 0 1 2 2 2
+    1 1 1       1 1 1
+    0 0 0       0 0 0
+    1 1 1       1 1 1
+    2 2 2 1 0 1 2 2 2
+    3 3 2 1 0 1 2 3 3
+    4 3 2 1 0 1 2 3 4
+    ```
+    and continuing outward arbitrarily.
     """
     if r < 2:
         raise ValueError("Radius must be greater than or equal to 2")
 
     if i == 0:
-        i = i + 8 * r # Transform by 1 rotation to avoid divide by zero
+        i = i + 8 * r # Translate by 1 rotation to avoid divide by zero
 
     s = r - 1
     return int(((-1)**(i / r)) * (s + ((2 * ceil(i / (2 * r))) * r)) - (((-1)**(i / r)) * i))
