a9d5163447dc16a9d23d7933c58b151215620183
gla3dr <zmayoh@gmail.com>
2021-12-02 01:33:35 -0600

    Add simplified solution for Day 1 part 2

--- 2021/Day1/problem2.py
+++ 2021/Day1/problem2.py
@@ -3,3 +3,6 @@ with open('2021/Day1/input1.txt', 'r') as i:
 
     # sums = [sum(w) for w in zip(input, input[1:], input[2:])]
     print(len([(a,b) for (a,b) in zip([sum(w) for w in zip(input, input[1:], input[2:])], [sum(w) for w in zip(input, input[1:], input[2:])][1:]) if b>a]))
+
+    # After realizing that (a+b+c < b+c+d) == (a < d) the solution becomes much simpler...
+    print(len([(x, y) for (x, y) in zip(input, input[3:]) if y > x]))
